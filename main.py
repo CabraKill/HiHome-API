132316c5fb728ed16a47e23fb6dd6cc3e1edec2a
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-09 15:49:39 -0300

    Update main with module lamp and infrastructure

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+from src.devices.lamp.lampIO import LampIO
 from src.service.firebase.firebaseService import FirebaseAPIService
 
 firebaseAPIService = FirebaseAPIService(project_name='home-dbb7e')
 
+lampDriver = LampIO(db_path='houses/buHkimoPE1e5NMx7C4kX/devices/F5fEOy8Yzctl31UZxu7Y',
+                        firebaseService=firebaseAPIService)
 
-def lamp(col_snapshot, changes, read_time):
-    for doc in col_snapshot:
-        print(f'{doc.id} - {doc.get("timee")}')
-
-
-firebaseAPIService.setActionForDocumentChange("configs/last", lamp)
 print('Press Ctrl+C to exit <3')
 while True:
     pass
