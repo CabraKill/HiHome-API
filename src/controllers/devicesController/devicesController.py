889733ac695c51a6f1ba0d025b1cf58559ff5483
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-10 16:05:38 -0300

    Update devicescontroller with init

--- src/controllers/devicesController/devicesController.py
+++ src/controllers/devicesController/devicesController.py
@@ -1,3 +1,4 @@
+from src.controllers.devicesController.models.deviceInitializationEntity import DeviceInitializationEntity
 from src.devices.lamp.lampIO import LampIO
 from src.service.firebase.Ifirebase import IFirebase
 from src import app
@@ -5,12 +6,21 @@ from flask import request
 
 
 class DevicesController:
-    def __init__(self) -> None:
+    def __init__(self, home_name: str, firebaseService: IFirebase) -> None:
+        self.home_name = home_name
+        self.firebaseService = firebaseService
         self.setEndpoints()
-    
+
     def deviceInitialization(self):
-        body = request.json
+        body_json = request.json
+        deviceInitializationEntity = DeviceInitializationEntity.fromJson(
+            body_json)
+        device = self.firebaseService.getCollection(
+            f'houses/{self.home_name}/devices').where('mac', '==', deviceInitializationEntity.mac)
+        self.firebaseService.up
+
         return 'OK'
 
     def setEndpoints(self):
-        app.add_url_rule('/init',view_func=self.deviceInitialization, methods=['POST'])
+        app.add_url_rule(
+            '/init', view_func=self.deviceInitialization, methods=['POST'])
