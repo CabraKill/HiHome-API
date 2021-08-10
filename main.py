a7b25dddb0630a60d2a2e0ced215825706833cd3
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-10 14:14:00 -0300

    Update main with deviceService

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from src import app
+from src.service.device.deviceService import DevicesService
 from src.controllers.devicesController.devicesController import DevicesController
 from src.devices.lamp.lampIO import LampIO
 from src.service.firebase.firebaseService import FirebaseAPIService
 
+
 firebaseAPIService = FirebaseAPIService(project_name='home-dbb7e')
 
-devicesController = DevicesController(firebaseAPIService)
+deviceService = DevicesService(firebaseAPIService)
+devicesController = DevicesController()
 
 print('Press Ctrl+C to exit <3')
-while True:
-    pass
-
-print("Done")
