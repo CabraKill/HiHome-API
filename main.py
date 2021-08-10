493e75aa61de43881e8f6a182646d4f97770c0f7
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-10 10:39:17 -0300

    Add devices initialization on main

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from src.controllers.devicesController.devicesController import DevicesController
 from src.devices.lamp.lampIO import LampIO
 from src.service.firebase.firebaseService import FirebaseAPIService
 
 firebaseAPIService = FirebaseAPIService(project_name='home-dbb7e')
 
-lampDriver = LampIO(db_path='houses/buHkimoPE1e5NMx7C4kX/devices/F5fEOy8Yzctl31UZxu7Y',
-                        firebaseService=firebaseAPIService)
+devicesController = DevicesController(firebaseAPIService)
 
 print('Press Ctrl+C to exit <3')
 while True:
