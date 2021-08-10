1e275e328617f9d6caa765a3f4972e0d7308a1b7
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-10 16:00:42 -0300

    Update firebase service interface

--- src/service/firebase/Ifirebase.py
+++ src/service/firebase/Ifirebase.py
@@ -1,7 +1,10 @@
 from google.cloud.firestore import Client
+from google.cloud.firestore_v1.base_document import DocumentSnapshot
+from google.cloud.firestore_v1.collection import CollectionReference
 from google.cloud.firestore_v1.document import DocumentReference
 from abc import ABC, abstractmethod
-from typing import Callable
+from typing import Any, Callable, Generator
+from src.service.firebase.models.documentEntity import DocumentFirebaseEntity
 
 
 class IFirebase(ABC):
@@ -20,9 +23,17 @@ class IFirebase(ABC):
         pass
 
     @abstractmethod
-    def getCollection(self, path: str):
+    def getCollection(self, path: str) -> CollectionReference:
+        pass
+
+    @abstractmethod
+    def getDocumentCollection(self, path: str) -> Generator[Any, Any, None]:
         pass
 
     @abstractmethod
     def setActionForDocumentChange(self, path: str, function: Callable):
+        pass
+
+    @abstractmethod
+    def updateDocument(self, id: str, document: DocumentFirebaseEntity):
         pass
