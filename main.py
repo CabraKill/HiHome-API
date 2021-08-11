242e8370513bd7d7331c01f9c3fb2f4cbc54a938
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-11 13:29:05 -0300

    Add HOME_NAME to main and correct DevicesController instantion

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from src.controllers.devicesController.devicesController import DevicesControlle
 from src.devices.lamp.lampIO import LampIO
 from src.service.firebase.firebaseService import FirebaseAPIService
 
+HOME_NAME = 'home-dbb7e'
 
-firebaseAPIService = FirebaseAPIService(project_name='home-dbb7e')
+
+firebaseAPIService = FirebaseAPIService(project_name=HOME_NAME)
 
 deviceService = DevicesService(firebaseAPIService)
-devicesController = DevicesController()
+devicesController = DevicesController(home_name=HOME_NAME,firebaseService=firebaseAPIService)
 
 print('Press Ctrl+C to exit <3')
