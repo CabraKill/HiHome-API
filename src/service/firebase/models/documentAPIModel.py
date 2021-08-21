e682d4a93e84de607982b9cd1d886f0cc9fa4cdf
raphael-linux <raphaeldesouza@outlook.com>
2021-08-21 01:39:48 -0300

    feat(documentAPIModel): add document initialization

--- src/service/firebase/models/documentAPIModel.py
+++ src/service/firebase/models/documentAPIModel.py
@@ -5,6 +5,7 @@ from google.cloud.firestore_v1.document import DocumentReference
 class DocumentFirebaseAPIModel(DocumentFirebaseEntity):
     def __init__(self, documentReference: DocumentReference) -> None:
         super().__init__(document=documentReference)
+        self.document = documentReference
 
     def get(self, key: str):
         value = self.document.get(key)
